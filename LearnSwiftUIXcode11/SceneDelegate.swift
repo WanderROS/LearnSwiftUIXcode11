40324a3906bf11101b634e6a2f7a7eef0b1bb339
WanderROS <wsp1194409532@gmail.com>
2021-10-22 22:34:19 +0800

    Xcode 多环境配置

--- LearnSwiftUIXcode11/SceneDelegate.swift
+++ LearnSwiftUIXcode11/SceneDelegate.swift
@@ -19,9 +19,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         #if DEV
-            print("!!!!!!DEV!!!!!!")
+            #if DEBUG
+                print("!!!!!!DEBUG!!!!!!")
+            #else
+                print("!!!!!!RELEASE!!!!!!")
+            #endif
         #else
-            print("!!!!!!ELSE!!!!!!")
+            print("!!!!!!ELSE ENV!!!!!!")
         #endif
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
