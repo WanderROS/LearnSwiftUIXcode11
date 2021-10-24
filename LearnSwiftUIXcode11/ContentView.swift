5ad5caa085127f1cbb3013efc9b58bafcace6c01
WanderROS <wsp1194409532@gmail.com>
2021-10-24 17:02:48 +0800

    combine 尝试

--- LearnSwiftUIXcode11/ContentView.swift
+++ LearnSwiftUIXcode11/ContentView.swift
@@ -7,26 +7,33 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
-    @State var text: String = "hello"
+    @State var username: String = ""
+    @State var password: String = ""
     var body: some View {
-        NavigationView{
-            NavigationLink(destination: MyViewController().navigationBarTitle("MyViewController",displayMode: .inline)){
-                Text("Hello")
+        VStack{
+            TextField("请输入用户名", text: $username)
+                .padding([.leading],25)
+                .padding([.bottom],15)
+            TextField("请输入密码", text: $password)
+                .padding([.leading],25)
+                .padding([.bottom],30)
+            Button(action: {
+                let publisher = Just(5).map { value -> Int in
+                    return value
+                }.sink { value in
+                    print("最终结果接收到的值为 \(value)")
+                }
+            }) {
+                Text("登录")
+                    .foregroundColor(.white)
             }
-        }
-    }
-}
+            .frame(width:120,height: 40)
+            .background(Color.blue)
 
-struct MyViewController: UIViewControllerRepresentable{
-    func makeUIViewController(context: Context) -> UIViewController {
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = UIColor.gray
-        return viewController
-    }
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-        
+        }
     }
 }
 
