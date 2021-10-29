5b5323291b6877fd7a0e25e65c9e57b569c23766
WanderROS <wsp1194409532@gmail.com>
2021-10-29 23:03:06 +0800

    swift 二维码生成

--- LearnSwiftUIXcode11/ContentView.swift
+++ LearnSwiftUIXcode11/ContentView.swift
@@ -9,13 +9,43 @@
 import SwiftUI
 import Combine
 
+import EFQRCode
+
 struct ContentView : View {
     @State private var selected = 3
+    @State private var image : CGImage?
+    @State private var dataUrl = Bundle.main.url(forResource: "QRCodeGIF6", withExtension: "gif")
     var body: some View {
             TabView(selection: $selected) {
                 VStack {
                     Text("hello")
                     Text("Nice")
+                    Button("Generate"){
+                        if let image = EFQRCode.generate(
+                            for: "https://github.com/EFPrefix/EFQRCode"
+                        ) {
+                            print("Create QRCode image success \(image)")
+                            self.image = image
+                        } else {
+                            print("Create QRCode image failed!")
+                        }
+                    }.padding()
+                    Button("Reconginze"){
+                        if let testImage = self.image {
+                            let codes = EFQRCode.recognize(testImage)
+                            if !codes.isEmpty {
+                                print("There are \(codes.count) codes")
+                                for (index, code) in codes.enumerated() {
+                                    print("The content of QR Code \(index) is \(code).")
+                                }
+                            } else {
+                                print("There is no QR Codes in testImage.")
+                            }
+                        }
+                    }
+                    if image != nil {
+                        Image(image!, scale: 2, label: Text("测试"))
+                    }
                 }
                     .tabItem {
                         Image(systemName: "1.square.fill")
--- Podfile
+++ Podfile
@@ -4,7 +4,8 @@
 target 'dev' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-
+  
+  pod 'EFQRCode', '~> 6.1.0'
   # Pods for dev
 
 end
