f57b71dc3dfe00afa704f910c7bd56466d3a0a58
WanderROS <wsp1194409532@gmail.com>
2021-11-08 22:48:16 +0800

    轮播图删减

--- LearnSwiftUIXcode11/ContentView.swift
+++ LearnSwiftUIXcode11/ContentView.swift
@@ -32,7 +32,7 @@ struct ContentView: View {
         let image: String
     }
 
-    private let images: [TutorialItem] = [
+    @State private var images: [TutorialItem] = [
         TutorialItem(image: "1"),
         TutorialItem(image: "2"),
         TutorialItem(image: "3"),
@@ -44,43 +44,53 @@ struct ContentView: View {
     
     @State var currentPage: Int = 0
     var body: some View {
-        ZStack(content: {
-            FSPagerViewSUI($currentPage, images) { item in
-                Image(item.image)
-                    .resizable()
-                    .cornerRadius(10)
-                    .frame(
-                        maxWidth: .infinity,
-                        maxHeight: .infinity
-                ).padding([.horizontal],5)
-            }
-            .itemSize(FSPagerView.automaticSize)
-            .decelerationDistance(1)
-            .didSelect { (value) in
-                print("按中\(value)")
-            }
-            .isInfinite(true)
-            .automaticSlidingInterval(3.0)
-            .transformer(FSPagerViewTransformer(type: .depth))
+        VStack{
+                    ZStack(content: {
+                        FSPagerViewSUI($currentPage, images) { item in
+                            Image(item.image)
+                                .resizable()
+                                .cornerRadius(10)
+                                .frame(
+                                    maxWidth: .infinity,
+                                    maxHeight: .infinity
+                            ).padding([.horizontal],5)
+                        }
+                        .itemSize(FSPagerView.automaticSize)
+                        .decelerationDistance(1)
+                        .didSelect { (value) in
+                            print("按中\(value)")
+                        }
+                        .isInfinite(true)
+                        .automaticSlidingInterval(3.0)
+                        .transformer(FSPagerViewTransformer(type: .depth))
 
-            VStack(alignment: .center, spacing: nil, content: {
-                FSPageControlSUI(currentPage: $currentPage)
-                    .numberOfPages(images.count)
-                    .contentHorizontalAlignment(.right)
-                    .contentInsets(UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20))
-                    .setFillColor(UIColor.systemPink, for: .selected)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 20)
-                    
-//                .background(Color.black.opacity(0.5))
-            })
-            .frame(
-                maxWidth: .infinity,
-                maxHeight: .infinity,
-                alignment: .bottomTrailing
-            )
-        })
-        .aspectRatio(375.0/193.0, contentMode: .fit)
+                        VStack(alignment: .center, spacing: nil, content: {
+                            FSPageControlSUI(currentPage: $currentPage)
+                                .numberOfPages(images.count)
+                                .contentHorizontalAlignment(.right)
+                                .contentInsets(UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20))
+                                .setFillColor(UIColor.systemPink, for: .selected)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 20)
+                                
+            //                .background(Color.black.opacity(0.5))
+                        })
+                        .frame(
+                            maxWidth: .infinity,
+                            maxHeight: .infinity,
+                            alignment: .bottomTrailing
+                        )
+                    })
+                    .aspectRatio(375.0/193.0, contentMode: .fit)
+            
+            Button("按下"){
+                self.images.append(TutorialItem(image: "find"))
+            }
+            Button("去掉"){
+                self.images.remove(at: self.images.count-1)
+            }
+        }
+
     }
 }
 
