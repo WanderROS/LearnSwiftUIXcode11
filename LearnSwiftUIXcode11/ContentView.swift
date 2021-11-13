2c4467e7417977101703dfbd37e1c8fa355a2a3e
WanderROS <wsp1194409532@gmail.com>
2021-11-13 11:30:24 +0800

    swift json

--- LearnSwiftUIXcode11/ContentView.swift
+++ LearnSwiftUIXcode11/ContentView.swift
@@ -12,8 +12,47 @@ import SwiftUI
 import FSPagerView
 import ObjectMapper
 import Moya
+import SwiftyJSON
+import ObjectMapper
+import HandyJSON
+
+struct RootClass: HandyJSON {
+    var date: String?
+    var stories: [Story]?
+    var topStories: [TopStory]?
+}
+
+
+struct TopStory: HandyJSON {
+    
+    var gaPrefix: String?
+    var hint: String?
+    var id: Int?
+    var image: String?
+    var imageHue: String?
+    var title: String?
+    var type: Int?
+    var url: String?
+        
+}
+
+struct Story: HandyJSON {
+    
+    var gaPrefix: String?
+    var hint: String?
+    var id: Int?
+    var imageHue: String?
+    var images: [String]?
+    var title: String?
+    var type: Int?
+    var url: String?
+        
+    
+}
+
 
 class AppData: ObservableObject {
+    
     
     @Published var fontColor = Color.black
     @Published var recipes = [RecipeModel]()
@@ -26,6 +65,51 @@ class AppData: ObservableObject {
         recipes.append(recipe)
     }
     
+}
+
+
+struct StudentModel {
+  var name: String?
+  var sex: String?
+  var age: Int?
+  var address: String?
+  var roomNum: Int?
+  var scores: Scores?
+  
+  init(_ jsonData: JSON) {
+    name     = jsonData["name"].stringValue
+    sex      = jsonData["sex"].stringValue
+    age      = jsonData["age"].intValue
+    address  = jsonData["address"].stringValue
+    roomNum  = jsonData["roomNum"].intValue
+    scores   = Scores(jsonData["scores"])
+  }
+    init(name: String?,sex: String?,age: Int?,address: String?, roomNum: Int? , scores: Scores?){
+        self.name = name
+        self.sex = sex
+        self.age = age
+        self.address = address
+        self.roomNum = roomNum
+        self.scores = scores
+    }
+}
+
+struct Scores {
+  var chinese: Int?
+  var math: Int?
+  var english: Int?
+
+    init(chinese:Int?,math:Int?,english:Int?){
+        self.chinese = chinese
+        self.math = math
+        self.english = english
+    }
+  
+  init(_ jsonData: JSON){
+    chinese = jsonData["chinese"].intValue
+    math    = jsonData["math"].intValue
+    english = jsonData["english"].intValue
+  }
 }
 
 // Model 定义
@@ -109,24 +193,14 @@ struct ContentView: View {
                     .aspectRatio(375.0/193.0, contentMode: .fit)
             
             Button("按下"){
-                //self.images.append(TutorialItem(image: "find"))
-//                self.cancelableRequest = NetWorkRequest(API.easyRequset, modelType: [ZhihuItemModel].self, successCallback: { (zhihuModels, responseModel) in
-//                            zhihuModels.forEach({ (item) in
-//                                print("模型属性--\(item.title ?? "模型无title")" )
-//                                print("图片链接--\(item.images?.first ?? "nil")")
-//                                let url = URL(string: item.images?.first ?? "nil")
-//                                if url != nil{
-//                                    let data = try? Data(contentsOf: url!)
-//                                    let temp =  TutorialItem(image: data!)
-//                                    self.images.append(temp)
-//                                }
-//
-//
-//                        })
-//                        }, failureCallback: { (responseModel) in
-//                            print("网络请求失败 包括服务器错误和网络异常\(responseModel.code)__\(responseModel.message)")
-//                        })
-            }
+                NetWorkRequest(API.easyRequset, successCallback: {
+                    data in
+                    let str=String(data:data,encoding:.utf8)!
+                    let root = RootClass.deserialize(from: str)
+                    print(root?.toJSONString())
+                    
+                })
+            }.padding().background(Color.red)
             Button("食物"){
                 DispatchQueue.global().async{
                     let url = URL(string: "https://source.unsplash.com/720x450/?food")
@@ -148,10 +222,10 @@ struct ContentView: View {
                         print("网络请求失败 包括服务器错误和网络异常")
                     })
                 
-            }
+            }.padding().background(Color.gray)
             Button("去掉"){
                 self.images.remove(at: self.images.count-1)
-            }
+            }.padding().background(Color.orange)
         }
 
     }
